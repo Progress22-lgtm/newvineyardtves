7e0ed0ee35f20ba322fce8b84b602312f91840c8
Your Name <you@example.com>
2020-11-14 14:27:50 +0100

    images relocated again with server changes ading express static

--- public/assets/js/galleyswap.js
+++ public/assets/js/galleyswap.js
@@ -3,7 +3,7 @@ const ctacss = cta.style
 var number = 1
 setInterval(function(){
     var picturetag = `picture(${number})`
-    cta.style.backgroundImage = `linear-gradient(rgba(206, 27, 40, 0.25), rgba(206, 27, 40, 0.25)), url('../../images/pictures/${picturetag}.JPG')`;
+    cta.style.backgroundImage = `linear-gradient(rgba(206, 27, 40, 0.25), rgba(206, 27, 40, 0.25)), url('/images/${picturetag}.JPG')`;
     number = number + 1
     if (number == 49) {
         number = 1
